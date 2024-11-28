29ed2e769e701f9d06283d2340dca846f9515b1f
Haku <polina231003@mail.ru>
2024-11-28 23:43:33 +0300

    exception handling

--- calculator/src/main/java/ru/neoflex/edu/java/controller/CalculatorController.java
+++ calculator/src/main/java/ru/neoflex/edu/java/controller/CalculatorController.java
@@ -6,6 +6,7 @@ import ru.neoflex.edu.java.dto.CreditDto;
 import ru.neoflex.edu.java.dto.LoanOfferDto;
 import ru.neoflex.edu.java.dto.LoanStatementRequestDto;
 import ru.neoflex.edu.java.dto.ScoringDataDto;
+import ru.neoflex.edu.java.exception.CalculatorException;
 import ru.neoflex.edu.java.service.CreditService;
 import ru.neoflex.edu.java.service.OfferService;
 import ru.neoflex.edu.java.service.ScoringService;
@@ -28,7 +29,7 @@ public class CalculatorController implements CalculatorApi {
     @Override
     public CreditDto calculateCredit(ScoringDataDto scoringDataDto) {
         if (!scoringService.checkSalary(scoringDataDto.employment().salary(), scoringDataDto.dependentAmount(), scoringDataDto.amount()))
-            throw new IllegalArgumentException("Bad salary");
+            throw new CalculatorException("Bad salary");
         return creditService.calculateCredit(scoringDataDto);
     }
 }
