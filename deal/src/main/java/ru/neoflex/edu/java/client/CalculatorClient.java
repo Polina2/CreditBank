974d12dcb0af8d13164529e02e9a84cbbcf7bc65
Haku <polina231003@mail.ru>
2024-12-18 22:25:07 +0300

    client

--- deal/src/main/java/ru/neoflex/edu/java/client/CalculatorClient.java
+++ deal/src/main/java/ru/neoflex/edu/java/client/CalculatorClient.java
@@ -1,7 +1,40 @@
 package ru.neoflex.edu.java.client;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import ru.neoflex.edu.java.dto.CreditDto;
+import ru.neoflex.edu.java.dto.LoanOfferDto;
+import ru.neoflex.edu.java.dto.LoanStatementRequestDto;
+import ru.neoflex.edu.java.dto.ScoringDataDto;
+
+import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class CalculatorClient {
+    private final WebClient calculatorWebClient;
+
+    public List<LoanOfferDto> getOffers(LoanStatementRequestDto request) {
+        String path = "/calculator/offers";
+        List<LoanOfferDto> response = calculatorWebClient
+                .post()
+                .uri(path)
+                .retrieve()
+                .bodyToFlux(LoanOfferDto.class)
+                .collectList()
+                .block();
+        return response;
+    }
+
+    public CreditDto calculateCredit(ScoringDataDto request) {
+        String path = "/calculator/calc";
+        CreditDto response = calculatorWebClient
+                .post()
+                .uri(path)
+                .retrieve()
+                .bodyToMono(CreditDto.class)
+                .block();
+        return response;
+    }
 }
--- deal/src/main/java/ru/neoflex/edu/java/controller/DealController.java
+++ deal/src/main/java/ru/neoflex/edu/java/controller/DealController.java
@@ -1,14 +1,18 @@
 package ru.neoflex.edu.java.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 import ru.neoflex.edu.java.dto.FinishRegistrationRequestDto;
 import ru.neoflex.edu.java.dto.LoanOfferDto;
 import ru.neoflex.edu.java.dto.LoanStatementRequestDto;
+import ru.neoflex.edu.java.service.StatementService;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class DealController implements DealApi{
+    private final StatementService statementService;
     @Override
     public List<LoanOfferDto> getOffers(LoanStatementRequestDto request) {
         return null;
