a3f279838be53caa625bb8b86e9059a006a42566
Haku <polina231003@mail.ru>
2025-01-05 16:30:38 +0300

    corrections

--- deal/src/main/java/ru/neoflex/edu/java/repository/JpaStatementRepository.java
+++ deal/src/main/java/ru/neoflex/edu/java/repository/JpaStatementRepository.java
@@ -3,7 +3,6 @@ package ru.neoflex.edu.java.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.neoflex.edu.java.entity.Statement;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaStatementRepository extends JpaRepository<Statement, UUID> {
